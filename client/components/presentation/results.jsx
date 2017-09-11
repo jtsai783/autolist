d08e046ce76a3a1081ab94e2d00136e70b7fc5b2
Jim Tsai <jim.h.tsai@gmail.com>
2017-09-11 11:33:03 -0700

    styling results

--- client/components/presentation/results.jsx
+++ client/components/presentation/results.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import '../styles/results.css'
 
-const ListItem = ({vin}) => (
-	<div>{vin}
+
+const ListItem = ({item}) => (
+	<div className="listitem">
+		<div className="imgcontainer">
+			<img className="primary-photo" src={item.primary_photo_url} />
+		</div>
+		<div className="details">
+			<div className="primary-details">
+				<div className="primary-left">
+					<div className="title">
+						{item.year} {item.make} {item.model}
+					</div>
+					<div className="mileage">{item.mileage}</div>
+				</div>
+				<div className="primary-right">
+					<div className="price">{item.price}</div>
+				</div>
+			</div>
+			<div className="secondary-details">
+				<div className="location">{item.city}, {item.state}</div>
+			</div>
+		</div>
 	</div>
 )
 
 const Results = ({ results }) => {
 
 	const listItems = results.map((result) => (
-		<li key={result.id}>
-			<ListItem vin={result.vin}/>
-		</li>
+		<ListItem key={result.id} item={result} />
 	));
 
 	return (
